3ed8b75de3762a940628c09906059cd6e72ab9c4
JayeshSardesai <desaijayash@gmail.com>
2025-03-30 08:25:22 +0530

    new commit

---
+++ pro13/middleware.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { NextResponse } from 'next/server'
+
+export function middleware(request) {
+    return NextResponse.redirect(new URL('/', request.url))
+}
+export const config = {
+    matcher: '/about/:path*',
+}
