94f6b159726022cd75269198898e6190184b61bc
JayeshSardesai <desaijayash@gmail.com>
2025-04-10 08:25:47 +0530

    This is new commit

---
+++ pro16/app/admin/adminLogout/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const adminLogout = () => {
+    return (
+        <div>
+            This is AdminLogout page
+        </div>
+    )
+}
+
+export default adminLogout
---
+++ pro16/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="homePage">
+      This is home page
+    </div>
+  );
+}
